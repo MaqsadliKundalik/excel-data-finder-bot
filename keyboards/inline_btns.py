fb578a4b077adbbd62f23cad67ce233e6e556a85
Odilbek <=>
2026-01-20 00:09:49 +0500

    c

--- keyboards/inline_btns.py
+++ keyboards/inline_btns.py
@@ -13,11 +13,11 @@ def search_results_btn(results: List[Medicines]):
 def del_medicine_btn(med_id: int):
     inline_markup = InlineKeyboardBuilder()
     inline_markup.button(text="O'chirish", callback_data=f"del_{med_id}")
-    inline_markup.adjust(1).as_markup() 
-    return inline_markup
+    inline_markup.adjust(1)
+    return inline_markup.as_markup() 
 
 def analogs_btn(med_code_atx: int):
     inline_markup = InlineKeyboardBuilder()
     inline_markup.button(text="Analoglari", callback_data=f"analogs_{med_code_atx}")
-    inline_markup.adjust(1).as_markup() 
-    return inline_markup
+    inline_markup.adjust(1)
+    return inline_markup.as_markup()
